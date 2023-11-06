0ce4b269b3f1f29f5d5b5de2b0008a82fda5ed6a
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-11-06 15:30:48 +0900

    init : 프로젝트 생성

---
+++ src/test/java/com/programmers/guitarshop/GuitarShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.guitarshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuitarShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
